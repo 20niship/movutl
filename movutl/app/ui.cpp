3b41226ecd8db60caebf287bc5d3f780f4b06736
20niship <idcnishimiya5378@gmail.com>
2025-01-02 04:12:41 +0900

    wip

--- movutl/app/ui.cpp
+++ movutl/app/ui.cpp
@@ -1,49 +1,106 @@
-#include <movutl/ui/ui.hpp>
-#include <movutl/instance/instance.hpp> 
+#pragma once
 
-namespace mu::ui {
+#include <movutl/app/ui.hpp>
+#include <stdio.h>
 
-Editor editor;
-Editor* get_editor_ptr() { return &editor; }
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
 
-void Editor::init() {
+#include <imgui.h>
+#include <imgui_impl_glfw.h>
+#include <imgui_impl_opengl3.h>
 
-  if(!glfwInit()) {
-    std::cerr << "ERROR: could not start GLFW3\n";
-    return;
-  }
-#ifdef WITH_OPENGL
+// ImGui - standalone example application for Glfw + OpenGL 3, using programmable pipeline
+// If you are new to ImGui, see examples/README.txt and documentation at the top of imgui.cpp.
+static void error_callback(int error, const char* description) {
+  fprintf(stderr, "Error %d: %s\n", error, description);
+}
+
+namespace mu {
+
+void GUIManager::init() {
+  glfwSetErrorCallback(error_callback);
+  if(!glfwInit()) return 1;
+  glfwWindowHint(GLFW_SAMPLES, 4);
   glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
-  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
-#else
-  glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
-#endif
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
+  glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); // To make MacOS happy; should not be needed
+  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+  GLFWwindow* window = glfwCreateWindow(1280, 720, "ImGui OpenGL3 example", NULL, NULL);
+  glfwMakeContextCurrent(window);
+
+  glewInit();
+
+  // Setup ImGui binding
+  ImGui_ImplOpenGL3_Init();
+  ImGui_ImplGlfw_InitForOpenGL(window, true);
+  glfw_window = window;
+
+  // Load FontsR
+  // (there is a default font, this is only if you want to change it. see extra_fonts/README.txt for more details)
+  // ImGuiIO& io = ImGui::GetIO();
+  // io.Fonts->AddFontDefault();
+  // io.Fonts->AddFontFromFileTTF("../../extra_fonts/Cousine-Regular.ttf", 15.0f);
+  // io.Fonts->AddFontFromFileTTF("../../extra_fonts/DroidSans.ttf", 16.0f);
+  // io.Fonts->AddFontFromFileTTF("../../extra_fonts/ProggyClean.ttf", 13.0f);
+  // io.Fonts->AddFontFromFileTTF("../../extra_fonts/ProggyTiny.ttf", 10.0f);
+  // io.Fonts->AddFontFromFileTTF("c:\\Windows\\Fonts\\ArialUni.ttf", 18.0f, NULL, io.Fonts->GetGlyphRangesJapanese());
 }
 
-bool Editor::update(bool) {
-  assert(windows.size() > 0);
+bool show_test_window = true;
+bool show_another_window = false;
+ImVec4 clear_color = ImColor(114, 144, 154);
+
+void update() {
+  auto window = GUIManager::Get()->glfw_window;
+
+  // Main loop
+  bool should_close = glfwWindowShouldClose(window);
+  ImGui_ImplGlfw_NewFrame();
+
+  // 1. Show a simple window
+  // Tip: if we don't call ImGui::Begin()/ImGui::End() the widgets appears in a window automatically called "Debug"
+  {
+    static float f = 0.0f;
+    ImGui::Text("Hello, world!");
+    ImGui::SliderFloat("float", &f, 0.0f, 1.0f);
+    ImGui::ColorEdit3("clear color", (float*)&clear_color);
+    if(ImGui::Button("Test Window")) show_test_window ^= 1;
+    if(ImGui::Button("Another Window")) show_another_window ^= 1;
+    ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
+  }
+
+  // 2. Show another simple window, this time using an explicit Begin/End pair
+  if(show_another_window) {
+    ImGui::SetNextWindowSize(ImVec2(200, 100), ImGuiCond_FirstUseEver);
+    ImGui::Begin("Another Window", &show_another_window);
+    ImGui::Text("Hello");
+    ImGui::End();
+  }
+
+  // 3. Show the ImGui test window. Most of the sample code is in ImGui::ShowTestWindow()
+  if(show_test_window) {
+    ImGui::SetNextWindowPos(ImVec2(650, 20), ImGuiCond_FirstUseEver);
+    ImGui::ShowTestWindow(&show_test_window);
+  }
+
+  // Rendering
+  int display_w, display_h;
+  glfwGetFramebufferSize(window, &display_w, &display_h);
+  glViewport(0, 0, display_w, display_h);
+  glClearColor(clear_color.x, clear_color.y, clear_color.z, clear_color.w);
+  glClear(GL_COLOR_BUFFER_BIT);
+  ImGui::Render();
+
+  glfwSwapBuffers(window);
   glfwPollEvents();
-  for(size_t i = 0; i < windows.size(); i++) windows[i]->drawFrame();
-  instance::get_io()->api_update();
-  /* for(auto &w : windows) w->drawFrame(verbose); */ // TODO: これでうまくいかん時がある？
-#if 0
-    (*renderer.get_device_ptr())->waitIdle();
-#endif
-  return !windows[0]->should_close();
 }
 
-void Editor::terminate(const bool verbose) {
-  if(verbose) {
-    std::cout << "terminating vkui......." << std::endl;
-  }
+void GUIManager::terminate() {
+  ImGui_ImplOpenGL3_Shutdown();
+  ImGui_ImplGlfw_Shutdown();
   glfwTerminate();
-  if(verbose) {
-    std::cout << "deleting windows ........" << std::endl;
-  }
-  for(auto&& w : windows) w->terminate();
-  if(verbose) {
-    std::cout << "deleting windows ........" << std::endl;
-  }
-  return;
 }
-} // namespace mu::ui
+
+} // namespace mu
--- movutl/core/quotanion.hpp
+++ movutl/core/quotanion.hpp
@@ -1,13 +1,17 @@
 #pragma once
 #include <movutl/core/vector.hpp>
-namespace mu::core {
+
+namespace mu {
 
 struct Quat {
+public:
   float x, y, z, w;
-  Quat() {x = y = z = w = 0;}
+  Quat() { x = y = z = w = 0; }
   Quat(float x, float y, float z, float w) : x(x), y(y), z(z), w(w) {}
   Quat conjugate(const Quat& q) const { return Quat(-q.x, -q.y, -q.z, q.w); }
-  Quat operator*(const Quat& q) const { return Quat(w * q.x - z * q.y + y * q.z + x * q.w, z * q.x + w * q.y - x * q.z + y * q.w, -y * q.x + x * q.y + w * q.z + z * q.w, -x * q.x - y * q.y - z * q.z + w * q.w); }
+  Quat operator*(const Quat& q) const {
+    return Quat(w * q.x - z * q.y + y * q.z + x * q.w, z * q.x + w * q.y - x * q.z + y * q.w, -y * q.x + x * q.y + w * q.z + z * q.w, -x * q.x - y * q.y - z * q.z + w * q.w);
+  }
   Vec3 rotate(const Vec3 v) const {
     auto vq = Quat(v[0], v[1], v[2], 0);
     auto cq = conjugate(*this);
@@ -16,4 +20,4 @@ struct Quat {
   }
 };
 
-} // namespace mu::core
+} // namespace mu
