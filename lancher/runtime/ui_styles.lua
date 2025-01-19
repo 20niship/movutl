78a5bc9cad25d281ef2aaa0ae280502ec99e1a64
20niship <idcnishimiya5378@gmail.com>
2025-01-19 22:52:23 +0900

    ui style

--- lancher/runtime/ui_styles.lua
+++ lancher/runtime/ui_styles.lua
@@ -27,7 +27,7 @@ end
 
 local style_dark2 = {
   colors = {
-    { imgui.ImGuiCol_.ImGuiCol_Text;                  spectrum.GRAY800 },
+    { imgui.ImGuiCol_.ImGuiCol_Text,                  spectrum.GRAY800 },
     { imgui.ImGuiCol_.ImGuiCol_TextDisabled,          spectrum.GRAY500 },
     { imgui.ImGuiCol_.ImGuiCol_WindowBg,              spectrum.GRAY100 },
     { imgui.ImGuiCol_.ImGuiCol_ChildBg,               imgui.ImVec4(0.00, 0.00, 0.00, 0.00) },
@@ -188,12 +188,73 @@ local style_dark4 = {
   end
 }
 
+-- https://github.com/ocornut/imgui/issues/707#issuecomment-678611331
+local style_dark5 = {
+  colors = {
+    { imgui.ImGuiCol_.ImGuiCol_Text,                  imgui.ImVec4(1.00, 1.00, 1.00, 1.00) },
+    { imgui.ImGuiCol_.ImGuiCol_TextDisabled,          imgui.ImVec4(0.50, 0.50, 0.50, 1.00) },
+    { imgui.ImGuiCol_.ImGuiCol_WindowBg,              imgui.ImVec4(0.13, 0.14, 0.15, 1.00) },
+    { imgui.ImGuiCol_.ImGuiCol_ChildBg,               imgui.ImVec4(0.13, 0.14, 0.15, 1.00) },
+    { imgui.ImGuiCol_.ImGuiCol_PopupBg,               imgui.ImVec4(0.13, 0.14, 0.15, 1.00) },
+    { imgui.ImGuiCol_.ImGuiCol_Border,                imgui.ImVec4(0.43, 0.43, 0.50, 0.50) },
+    { imgui.ImGuiCol_.ImGuiCol_BorderShadow,          imgui.ImVec4(0.00, 0.00, 0.00, 0.00) },
+    { imgui.ImGuiCol_.ImGuiCol_FrameBg,               imgui.ImVec4(0.25, 0.25, 0.25, 1.00) },
+    { imgui.ImGuiCol_.ImGuiCol_FrameBgHovered,        imgui.ImVec4(0.38, 0.38, 0.38, 1.00) },
+    { imgui.ImGuiCol_.ImGuiCol_FrameBgActive,         imgui.ImVec4(0.67, 0.67, 0.67, 0.39) },
+    { imgui.ImGuiCol_.ImGuiCol_TitleBg,               imgui.ImVec4(0.08, 0.08, 0.09, 1.00) },
+    { imgui.ImGuiCol_.ImGuiCol_TitleBgActive,         imgui.ImVec4(0.08, 0.08, 0.09, 1.00) },
+    { imgui.ImGuiCol_.ImGuiCol_TitleBgCollapsed,      imgui.ImVec4(0.00, 0.00, 0.00, 0.51) },
+    { imgui.ImGuiCol_.ImGuiCol_MenuBarBg,             imgui.ImVec4(0.14, 0.14, 0.14, 1.00) },
+    { imgui.ImGuiCol_.ImGuiCol_ScrollbarBg,           imgui.ImVec4(0.02, 0.02, 0.02, 0.53) },
+    { imgui.ImGuiCol_.ImGuiCol_ScrollbarGrab,         imgui.ImVec4(0.31, 0.31, 0.31, 1.00) },
+    { imgui.ImGuiCol_.ImGuiCol_ScrollbarGrabHovered,  imgui.ImVec4(0.41, 0.41, 0.41, 1.00) },
+    { imgui.ImGuiCol_.ImGuiCol_ScrollbarGrabActive,   imgui.ImVec4(0.51, 0.51, 0.51, 1.00) },
+    { imgui.ImGuiCol_.ImGuiCol_CheckMark,             imgui.ImVec4(0.11, 0.64, 0.92, 1.00) },
+    { imgui.ImGuiCol_.ImGuiCol_SliderGrab,            imgui.ImVec4(0.11, 0.64, 0.92, 1.00) },
+    { imgui.ImGuiCol_.ImGuiCol_SliderGrabActive,      imgui.ImVec4(0.08, 0.50, 0.72, 1.00) },
+    { imgui.ImGuiCol_.ImGuiCol_Button,                imgui.ImVec4(0.25, 0.25, 0.25, 1.00) },
+    { imgui.ImGuiCol_.ImGuiCol_ButtonHovered,         imgui.ImVec4(0.38, 0.38, 0.38, 1.00) },
+    { imgui.ImGuiCol_.ImGuiCol_ButtonActive,          imgui.ImVec4(0.67, 0.67, 0.67, 0.39) },
+    { imgui.ImGuiCol_.ImGuiCol_Header,                imgui.ImVec4(0.22, 0.22, 0.22, 1.00) },
+    { imgui.ImGuiCol_.ImGuiCol_HeaderHovered,         imgui.ImVec4(0.25, 0.25, 0.25, 1.00) },
+    { imgui.ImGuiCol_.ImGuiCol_HeaderActive,          imgui.ImVec4(0.67, 0.67, 0.67, 0.39) },
+    { imgui.ImGuiCol_.ImGuiCol_Separator,             imgui.ImVec4(0.43, 0.43, 0.50, 0.50) },
+    { imgui.ImGuiCol_.ImGuiCol_SeparatorHovered,      imgui.ImVec4(0.41, 0.42, 0.44, 1.00) },
+    { imgui.ImGuiCol_.ImGuiCol_SeparatorActive,       imgui.ImVec4(0.26, 0.59, 0.98, 0.95) },
+    { imgui.ImGuiCol_.ImGuiCol_ResizeGrip,            imgui.ImVec4(0.00, 0.00, 0.00, 0.00) },
+    { imgui.ImGuiCol_.ImGuiCol_ResizeGripHovered,     imgui.ImVec4(0.29, 0.30, 0.31, 0.67) },
+    { imgui.ImGuiCol_.ImGuiCol_ResizeGripActive,      imgui.ImVec4(0.26, 0.59, 0.98, 0.95) },
+    { imgui.ImGuiCol_.ImGuiCol_Tab,                   imgui.ImVec4(0.08, 0.08, 0.09, 0.83) },
+    { imgui.ImGuiCol_.ImGuiCol_TabHovered,            imgui.ImVec4(0.33, 0.34, 0.36, 0.83) },
+    -- {imgui.ImGuiCol_.ImGuiCol_TabActive,            imgui.ImVec4(0.23, 0.23, 0.24, 1.00)},
+    -- {imgui.ImGuiCol_.ImGuiCol_TabUnfocused,         imgui.ImVec4(0.08, 0.08, 0.09, 1.00)},
+    -- {imgui.ImGuiCol_.ImGuiCol_TabUnfocusedActive,   imgui.ImVec4(0.13, 0.14, 0.15, 1.00)},
+    { imgui.ImGuiCol_.ImGuiCol_DockingPreview,        imgui.ImVec4(0.26, 0.59, 0.98, 0.70) },
+    { imgui.ImGuiCol_.ImGuiCol_DockingEmptyBg,        imgui.ImVec4(0.20, 0.20, 0.20, 1.00) },
+    { imgui.ImGuiCol_.ImGuiCol_PlotLines,             imgui.ImVec4(0.61, 0.61, 0.61, 1.00) },
+    { imgui.ImGuiCol_.ImGuiCol_PlotLinesHovered,      imgui.ImVec4(1.00, 0.43, 0.35, 1.00) },
+    { imgui.ImGuiCol_.ImGuiCol_PlotHistogram,         imgui.ImVec4(0.90, 0.70, 0.00, 1.00) },
+    { imgui.ImGuiCol_.ImGuiCol_PlotHistogramHovered,  imgui.ImVec4(1.00, 0.60, 0.00, 1.00) },
+    { imgui.ImGuiCol_.ImGuiCol_TextSelectedBg,        imgui.ImVec4(0.26, 0.59, 0.98, 0.35) },
+    { imgui.ImGuiCol_.ImGuiCol_DragDropTarget,        imgui.ImVec4(0.11, 0.64, 0.92, 1.00) },
+    -- {imgui.ImGuiCol_.ImGuiCol_NavHighlight,         imgui.ImVec4(0.26, 0.59, 0.98, 1.00)},
+    { imgui.ImGuiCol_.ImGuiCol_NavWindowingHighlight, imgui.ImVec4(1.00, 1.00, 1.00, 0.70) },
+    { imgui.ImGuiCol_.ImGuiCol_NavWindowingDimBg,     imgui.ImVec4(0.80, 0.80, 0.80, 0.20) },
+    { imgui.ImGuiCol_.ImGuiCol_ModalWindowDimBg,      imgui.ImVec4(0.80, 0.80, 0.80, 0.35) },
+  },
+  -- @param style: imgui.ImGuiStyle
+  func = function(style)
+    style.FrameRounding = 2.3
+    style.GrabRounding = 2.3
+  end
+}
 
 local function init_color_schemes()
   impl_register_style("dark2", style_dark2)
   impl_register_style("dark3", style_dark3)
   impl_register_style("dark4", style_dark4)
-  movutl.apply_imgui_style("dark4")
+  impl_register_style("dark5", style_dark5)
+  movutl.apply_imgui_style("dark5")
 end
 
 return init_color_schemes
